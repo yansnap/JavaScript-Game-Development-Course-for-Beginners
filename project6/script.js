6c54f3a9dff7f23438607cdc0df79af4d76fd024
JanHappy <jan.kozhyn@gmail.com>
2022-08-04 01:13:55 +0300

    create project 7

--- project6/script.js
+++ project6/script.js
@@ -2,3 +2,30 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d')
 canvas.width = 500;
 canvas.height = 800;
+
+class Game {
+    constructor() {
+        this.enemies = [];
+    }
+    update() {
+
+    }
+    draw() {
+
+    }
+    #addNewEnemy() {
+
+    }
+}
+
+class Enemy {
+    constructor() {
+
+    }
+    update() {
+
+    }
+    draw() {
+        
+    }
+}
