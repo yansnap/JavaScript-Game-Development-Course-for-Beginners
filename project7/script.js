4086b7759e8dacc914a27bd8731ba4c91cc94b35
JanHappy <yan.kozhyn@gmail.com>
2022-08-14 04:21:29 +0300

    end Project 8: State management in JavaScript games

--- project7/script.js
+++ project7/script.js
@@ -329,7 +329,7 @@ window.addEventListener('load', function () {
     lastTime = timeStamp;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     background.draw(ctx);
-    // background.update();
+    background.update();
     player.draw(ctx);
     player.update(input, deltaTime, enemies);
     handleEnemies(deltaTime);
